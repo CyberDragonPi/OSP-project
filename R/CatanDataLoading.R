9d8db46c118295ae887429e4e1b07e700739a849
CyberDragonPi <marko.pongrac@mail.inet.hr>
2024-01-21 22:41:35 +0100

    Game winners

--- R/CatanDataLoading.R
+++ R/CatanDataLoading.R
@@ -4,4 +4,6 @@ library(dplyr)
 library(goftest)
 
 catanData <- read.csv('data/SettlersOfCatanStats.csv')
-head(catanData) %>% print()
+#head(catanData) %>% print()
+
+game_winner_data <- catanData[catanData$points >= 10, ]
