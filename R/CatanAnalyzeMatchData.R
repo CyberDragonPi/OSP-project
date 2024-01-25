5befece9d52f38726216a4d8c7b4363ad5f6ba91
CyberDragonPi <marko.pongrac@mail.inet.hr>
2024-01-25 15:15:22 +0100

    Started working on dice fairness

--- R/CatanAnalyzeMatchData.R
+++ R/CatanAnalyzeMatchData.R
@@ -1,4 +1,13 @@
-print(getwd())
+library(tidyverse) 
+library(ggplot2)
+library(dplyr)
+library(goftest)
+
+#print(getwd())
 source("R/CatanDataLoading.R")
-all_variables <- ls()
-print(all_variables)
+#all_variables <- ls()
+#print(all_variables)
+
+dice_frequency <- game_winner_data[, c("X2", "X3", "X4","X5", "X6", "X7", "X8", "X9", "X10", "X11", "X12")]
+#nrow(dice_frequency) %>% print()
+#ncol(dice_frequency) %>% print()
